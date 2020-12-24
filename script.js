868194a59ab2c8d46d3c810c3683a4d7f5c5f55e
Dao Chen Yuan <daochenyuan@gmail.com>
2020-12-24 01:12:50 +0000

    Added undo button, moveList, changed handlers to be toggled by a global var, made scope of most functions higher

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 let instructions;
 let start;
+let undo;
 let cell = new Array(3);
 let cur;
 let s;
 let counter;
+let toggleClick = false;
+let toggleStarted = false;
+let toggleReset = true;
+let moveList = new Array(9);
 
 function handleLoad () {
-  function startUp () {
-    function onClick (i,j) {
-      function checkWin () {
-        let win = new Array(8);
-        for (let i = 0; i <= 2; i++) {
-          win[i] = [cell[i][0].textContent,
-          cell[i][1].textContent, cell[i][2].textContent];
-          win[i+3] = [cell[0][i].textContent,
-          cell[1][i].textContent, cell[2][i].textContent];
+  function checkWin () {
+    let win = new Array(8);
+    for (let i = 0; i <= 2; i++) {
+      win[i] = [cell[i][0].textContent,
+      cell[i][1].textContent, cell[i][2].textContent];
+      win[i+3] = [cell[0][i].textContent,
+      cell[1][i].textContent, cell[2][i].textContent];
+    }
+    win[6] = [cell[0][0].textContent,
+    cell[1][1].textContent, cell[2][2].textContent];
+    win[7] = [cell[2][0].textContent,
+    cell[1][1].textContent, cell[0][2].textContent];
+    let winning = false;
+    for (let i = 0; i <= 7; i++) {
+      winning = true;
+      for (let j = 0; j <= 2; j++) {
+        if (win[i][j] !== s) {
+          winning = false;
+          break;
         }
-        win[6] = [cell[0][0].textContent,
-        cell[1][1].textContent, cell[2][2].textContent];
-        win[7] = [cell[2][0].textContent,
-        cell[1][1].textContent, cell[0][2].textContent];
-        let winning = false;
-        for (let i = 0; i <= 7; i++) {
-          winning = true;
-          for (let j = 0; j <= 2; j++) {
-            if (win[i][j] !== s) {
-              winning = false;
-              break;
-            }
-          }
-          if (winning) {
-            handleClick = (i,j) => null;
-            for (let i = 0; i <= 2; i++) {
-              for (let j = 0; j <= 2; j++) {
-                if (cell[i][j].textContent === "") {
-                  cell[i][j].classList.remove("movable");
-                }
-              }
-            }
-            instructions.textContent =
-            `${s} won! Click "Reset" to start a new game!`;
-            return;
-          }
-        }
-        if (counter === 9) {
-            handleClick = (i,j) => null;
-            instructions.textContent = "It's a draw!";
-            return;
-        }        
       }
-      if (cell[i][j].textContent !== "") {
+      if (winning) {
+        toggleClick = false;
+        toggleStarted = false;
+        for (let i = 0; i <= 2; i++) {
+          for (let j = 0; j <= 2; j++) {
+            if (cell[i][j].textContent === "") {
+              cell[i][j].classList.remove("movable");
+            }
+          }
+        }
+        instructions.textContent =
+        `${s} won! Click "Reset" to start a new game!`;
         return;
       }
-      let opp;
-      if (s === "X") {
-        opp = "O";
-      } else {
-        opp = "X";
-      }
-      cell[i][j].textContent = s;
-      cell[i][j].classList.add("clicked");
-      instructions.textContent = opp + "'s turn!";
-      counter++;
-      if (counter >= 5) {
-        checkWin();
-      }
-      s = opp;
     }
+    if (counter === 9) {
+      toggleClick = false;
+      toggleStarted = false;
+      instructions.textContent =
+      `It's a draw! Click "Reset" to start a new game!`;
+      return;
+    }        
+  }  
+  function onClick (i,j) {
+    if (cell[i][j].textContent !== "") {
+      return;
+    }
+    let opp;
+    if (s === "X") {
+      opp = "O";
+    } else {
+      opp = "X";
+    }
+    cell[i][j].textContent = s;
+    cell[i][j].classList.add("clicked");
+    instructions.textContent = opp + "'s turn!";
+    moveList[counter] = [i,j];
+    counter++;
+    if (counter >= 5) {
+      checkWin();
+    }
+    s = opp;
+  }
+  
+  function startUp () {
     for (let i = 0; i <= 2; i++) {
       for (let j = 0; j <= 2; j++) {
         cell[i][j].textContent = "";
@@ -74,15 +84,17 @@ function handleLoad () {
       }
     }
     counter = 0;
-    handleStart = reset;
-    handleClick = (i,j) => onClick(i,j);
+    toggleStarted = true;
+    toggleClick = true;
+    toggleReset = false;
     start.textContent = "Reset";
     instructions.textContent = s + "'s turn!";
   }
 
   function reset () {
-    handleStart = startUp;
-    handleClick = (i,j) => null;
+    toggleStarted = false;
+    toggleClick = false;
+    toggleReset = true;
     s = "X";
     for (let i = 0; i <= 2; i++) {
       for (let j = 0; j <= 2; j++) {
@@ -93,9 +105,27 @@ function handleLoad () {
     start.textContent = "Start"
     instructions.textContent = 'Click "Start" to start a game!'
   }
+
+  function doUndo () {
+    if (counter === 0) {
+      return;
+    } else {
+      counter--;
+      let tmp = moveList[counter];
+      moveList[counter] = undefined;
+      console.log(moveList);
+    }
+  }
+
+
   s = "X";
-  let handleStart = startUp;
-  let handleClick = (i,j) => null;
+  const handleStart = () => {if (toggleReset) {startUp();}
+                              else {reset();}}
+  const handleClick = (i,j) => {if (toggleClick) {onClick(i,j);}
+                              else {return;}}
+  const handleUndo = () => {
+    if (toggleStarted) {doUndo();} else {return;}
+  }
   for (let i = 0; i <= 2; i++) {
     cell[i] = new Array(3);
     for (let j = 0; j <= 2; j++) {
@@ -106,5 +136,7 @@ function handleLoad () {
   instructions = document.getElementById('instructions');
   start = document.getElementById('start');
   start.addEventListener('click', () => handleStart());
+  undo = document.getElementById('undo');
+  undo.addEventListener('click', () => handleUndo());
 }
 window.addEventListener('load', handleLoad);
